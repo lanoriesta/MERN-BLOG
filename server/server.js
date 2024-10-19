e8d2106ca056a0b8d5f3be4e15fc870709bd9483
Lawrence Angelo Noriesta <lanoriesta@gmail.com>
2024-10-20 03:11:59 +0800

    fix configuration sendFile() in server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(__dirname, "client", "dist", "index.html");
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
